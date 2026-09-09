75a8d58c99b8d78e969954f00108d8401ac9fb8d
Vidit Jain <143209026+VIDITJAIN7@users.noreply.github.com>
2026-09-09 13:41:15 +0400

    Add out-of-fold Cleanlab label detector

--- detection/cleanlab_labels.py
+++ detection/cleanlab_labels.py
@@ -6,7 +6,7 @@ from cleanlab.filter import find_label_issues
 
 import config
 from models.resnet import build_resnet18
-from utils.data_loader import PoisonedDataset, get_train_transform
+from utils.data_loader import get_train_transform, get_test_transform
 from utils.train_eval import train_model, predict_indexed_probabilities
 
 
@@ -26,15 +26,26 @@ def detect_label_issues_oof(base_dataset, observed_labels, model_factory=build_r
     fold_ids = np.array_split(order, folds)
     probs = np.zeros((n, 10), dtype=np.float32)
 
+    class TransformedView:
+        def __init__(self, source, transform):
+            self.source, self.transform = source, transform
+        def __len__(self):
+            return len(self.source)
+        def __getitem__(self, idx):
+            item = self.source[idx]
+            return self.transform(item[0]), item[1], item[2] if len(item) > 2 else idx
+
     for held_out in fold_ids:
         train_ids = np.setdiff1d(np.arange(n), held_out, assume_unique=False)
         train_loader = DataLoader(
-            Subset(base_dataset, train_ids), batch_size=config.BATCH_SIZE,
+            Subset(TransformedView(base_dataset, get_train_transform()), train_ids),
+            batch_size=config.BATCH_SIZE,
             shuffle=True, num_workers=config.NUM_WORKERS,
             pin_memory=config.PIN_MEMORY, persistent_workers=config.NUM_WORKERS > 0,
         )
         test_loader = DataLoader(
-            Subset(base_dataset, held_out), batch_size=config.EVAL_BATCH_SIZE,
+            Subset(TransformedView(base_dataset, get_test_transform()), held_out),
+            batch_size=config.EVAL_BATCH_SIZE,
             shuffle=False, num_workers=config.NUM_WORKERS,
             pin_memory=config.PIN_MEMORY, persistent_workers=config.NUM_WORKERS > 0,
         )
